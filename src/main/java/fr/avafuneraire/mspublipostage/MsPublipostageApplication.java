1d15cc8c7e542c6ee562cacd3915627999b77f5a
Corentin Bringer <corentinbringer@ik.me>
2024-12-04 11:43:20 +0100

    first poc

---
+++ src/main/java/fr/avafuneraire/mspublipostage/MsPublipostageApplication.java
@@ -0,0 +1,13 @@
+package fr.avafuneraire.mspublipostage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPublipostageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPublipostageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/avafuneraire/mspublipostage/MsPublipostageApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.avafuneraire.mspublipostage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPublipostageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
